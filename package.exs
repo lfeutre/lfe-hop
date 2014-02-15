f3a6d49040279b72a82401d3704244e3a8d864ef
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2014-02-15 15:10:06 -0600

    Updated package info.

--- package.exs
+++ package.exs
@@ -1,9 +1,9 @@
-Expm.Package.new(name: "lfe-hop",
-                 description: "A Hierarchical Ordered Planner for LFE",
-                 version: "0.0.1",
-                 keywords: ["LFE", "Lisp", "Library", "AI",
-                            "Artificial Intelligence", "Planners",
-                            "Task Networks", "Automated Planning"],
-                 maintainers: [[name: "Duncan McGreggor",
-                                email: "oubiwann@cogitat.io"]],
-                 repositories: [[github: "oubiwann/lfe-hop"]])
+Expm.Package.new(
+  name: "lfe-hop",
+  description: "A Hierarchical Ordered Planner for LFE",
+  version: "0.0.1",
+  keywords: ["LFE", "Lisp", "Library", "AI", "Artificial Intelligence",
+             "Planners", "Task Networks", "Automated Planning"],
+  maintainers: [[name: "Duncan McGreggor",
+                 email: "oubiwann@cogitat.io"]],
+  repositories: [[github: "lfe/lfe-hop"]])
